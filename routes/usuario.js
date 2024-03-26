accb19906f8b02e124886ac051b8f0d9f7e70a75
GokuPrograming <verafrancomiguel1d@gmail.com>
2024-03-26 01:49:15 -0600

    se agregaron nuevas rutas NO FUNCIONALES, ademas de busqueda por id de usuario

--- routes/usuario.js
+++ routes/usuario.js
@@ -15,4 +15,22 @@ router.get('/usuario', async (req, res) => {
         res.send("Error " + err);
     }
 });
+router.get('/usuario/:id', async (req, res) => {
+    const id_usuario = req.params.id;  // Obtener el id_usuario desde los parámetros de la URL
+
+    try {
+        const client = await db.connect();
+
+        // Consulta SQL modificada para filtrar por id_usuario
+        const result = await client.query('SELECT * FROM usuario WHERE id_usuario = $1', [id_usuario]);
+
+        const results = { 'data': (result) ? result.rows : null };
+        res.json(results);
+
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
+});
 module.exports = router;
