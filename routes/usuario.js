f522d365209d5e1663f2df33cb8d45819184cb9c
GokuPrograming <verafrancomiguel1d@gmail.com>
2024-03-26 01:58:57 -0600

    agregamos login

--- routes/usuario.js
+++ routes/usuario.js
@@ -20,10 +20,8 @@ router.get('/usuario/:id', async (req, res) => {
 
     try {
         const client = await db.connect();
-
         // Consulta SQL modificada para filtrar por id_usuario
         const result = await client.query('SELECT * FROM usuario WHERE id_usuario = $1', [id_usuario]);
-
         const results = { 'data': (result) ? result.rows : null };
         res.json(results);
 
@@ -33,4 +31,28 @@ router.get('/usuario/:id', async (req, res) => {
         res.send("Error " + err);
     }
 });
+router.post('/usuario/login', async (req, res) => {
+    const { correo, password } = req.body;  // Obtener correo y password desde el cuerpo de la solicitud
+
+    try {
+        const client = await db.connect();
+
+        // Consulta SQL para verificar las credenciales del usuario
+        const result = await client.query('SELECT * FROM usuario WHERE correo = $1 AND password = $2', [correo, password]);
+
+        if (result.rows.length > 0) {
+            const user = result.rows[0];
+            res.json({ 'message': 'Inicio de sesión exitoso', 'data': user });
+        } else {
+            res.status(401).json({ 'message': 'Correo o contraseña incorrectos' });
+        }
+
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error " + err);
+    }
+});
+
+
 module.exports = router;
