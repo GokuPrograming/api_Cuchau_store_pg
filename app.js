be0d12bfad17dfe9bb8400c6dad09fa08062196c
GokuPrograming <verafrancomiguel1d@gmail.com>
2024-03-26 01:32:13 -0600

    se agrego una nueva ruta para usuarios

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const port = 3000
 
 //exportar rutas
 const rol = require('./routes/rol')
+const user = require('./routes/usuario')
 // express json
 const app = express()
 app.use(express.json())
 //uso de las rutas
-app.use('/',rol)
+app.use('/',rol,user)
 app.listen(port, async() => console.log(`Example app listening on port ${port}!`))
---
+++ routes/usuario.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const db = require('../database/connection')
+const router = express.Router()
+
+
+router.get('/usuario', async (req, res) => {
+    try {
+        const client = await db.connect();
+        const result = await client.query('SELECT * FROM usuario');
+        const results = { 'data': (result) ? result.rows : null };
+        res.json(results);
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
+});
+module.exports = router;
