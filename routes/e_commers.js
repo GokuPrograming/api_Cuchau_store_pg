dd5b6989c67f5b870d65ac3f283c219dbe489fc3
GokuPrograming <verafrancomiguel1d@gmail.com>
2024-04-01 21:41:55 -0600

    se agregaron validaciones en la ruta que sirve para actualizar los valores de la tienda

--- routes/e_commers.js
+++ routes/e_commers.js
@@ -4,6 +4,9 @@ const jwt = require('jsonwebtoken');
 const db = require('../database/connection');
 const router = express.Router();
 
+
+
+
 router.post('/login', async (req, res) => {
     const { correo, password } = req.body;
 
@@ -14,7 +17,7 @@ router.post('/login', async (req, res) => {
 
         if (result.rows.length > 0) {
             const user = result.rows[0];
-            const token = jwt.sign({ id_usuario: user.id_usuario, correo: user.correo,id_rol:user.id_rol }, process.env.JWT_SECRET || 'secreto');
+            const token = jwt.sign({ id_usuario: user.id_usuario, correo: user.correo, id_rol: user.id_rol }, process.env.JWT_SECRET || 'secreto');
 
             res.json({ message: 'Inicio de sesión exitoso', token, user });
         } else {
@@ -85,27 +88,127 @@ const validarToken = (req, res) => {
     }
 };
 
+// router.post('/addToCar', async (req, res) => {
+//     const { id_producto, id_usuario, cantidad } = req.body;
+//     console.log("Producto y Usuario:", id_producto, id_usuario, "cantidad", cantidad);
+
+//     // Validar el token
+//     const resultadoValidacion = validarToken(req, res);
+//     if (!resultadoValidacion.id_usuario) {
+//         return;
+//     }
+
+//     let client;
+//     try {
+//         client = await db.connect();
+//         // Obtener el stock del producto
+//         const almacen_row = await client.query('SELECT * FROM producto WHERE id_producto=$1', [id_producto]);
+//         // const stock = almacen_row.rows[0].almacen;
+//         // console.log("en almacen =", stock);
+//         let stock = 1;
+//         // Verificar si hay suficiente stock
+//         try {
+//             if (cantidad > stock) {
+//                 console.error("no hay suficiente stock");
+//                 console.error(error.message);
+//                 res.status(400).json({ message: 'No tenemos ya de esos productos :_' });
+
+//             } else {
+//                 await client.query('BEGIN');
+//                 // Buscar el precio del artículo
+//                 const { rows } = await client.query('SELECT precio FROM producto WHERE id_producto = $1', [id_producto]);
+//                 const subtotal = rows[0].precio * cantidad;
+
+//                 // Insertar el producto en el carrito
+//                 const result = await client.query(
+//                     'INSERT INTO carrito (id_usuario, id_producto, cantidad, subtotal, fecha_insertado) VALUES ($1, $2, $3, $4, NOW()) RETURNING *',
+//                     [id_usuario, id_producto, cantidad, subtotal]
+//                 );
+//                 await client.query(
+//                     'update producto set almacen=almacen-$1 where id_producto=$2', [cantidad, id_producto]
+//                 );
+
+//                 await client.query('COMMIT');
+//                 console.log('Producto insertado:', result.rows[0]);
+//                 res.status(200).json({ message: 'Producto agregado al carrito exitosamente' });
+//             }
+//         } catch (error) {
+//             console.error(error.message);
+
+//             if (client) {
+//                 await client.query('ROLLBACK'); // Si ocurre un error, realizar un rollback
+//                 client.release();
+//             }
+
+//             res.status(500).json({ message: 'Error al agregar el producto al carrito' });
+//         } finally {
+//             if (client) {
+//                 client.release();
+//             }
+//         }
+//     } catch { }
+
+// });
+
 router.post('/addToCar', async (req, res) => {
-    const { id_producto, id_usuario } = req.body;
-    console.log("Producto y Usuario:", id_producto, id_usuario);
+    const { id_producto, id_usuario, cantidad } = req.body;
+    console.log("Producto y Usuario:", id_producto, id_usuario, "cantidad", cantidad);
+
+    // Validar el token
     const resultadoValidacion = validarToken(req, res);
     if (!resultadoValidacion.id_usuario) {
         return;
     }
+
+    let client;
     try {
-        const client = await db.connect();
+        client = await db.connect();
+        
+        // Obtener el stock del producto
+        const almacen_row = await client.query('SELECT * FROM producto WHERE id_producto=$1', [id_producto]);
+        let stock = almacen_row.rows[0].almacen;
+
+        // Verificar si hay suficiente stock
+        if (cantidad > stock) {
+            console.error("no hay suficiente stock");
+            res.status(400).json({ message: 'No tenemos ya de esos productos :_' });
+            return;  // Agrega este return para salir de la función después de enviar la respuesta
+        }
+
+        await client.query('BEGIN');
+        // Buscar el precio del artículo
+        const { rows } = await client.query('SELECT precio FROM producto WHERE id_producto = $1', [id_producto]);
+        const subtotal = rows[0].precio * cantidad;
+
+        // Insertar el producto en el carrito
         const result = await client.query(
-            'INSERT INTO carrito (id_usuario, id_producto, cantidad, subtotal, fecha_insertado) VALUES ($1, $2, 1, (SELECT precio FROM producto WHERE id_producto = $2), NOW()) RETURNING *',
-            [id_usuario, id_producto]
+            'INSERT INTO carrito (id_usuario, id_producto, cantidad, subtotal, fecha_insertado) VALUES ($1, $2, $3, $4, NOW()) RETURNING *',
+            [id_usuario, id_producto, cantidad, subtotal]
         );
-        console.log('Producto insertado:', result.rows[0]);  // Agregar este console log
-        client.release();
+        await client.query(
+            'update producto set almacen=almacen-$1 where id_producto=$2', [cantidad, id_producto]
+        );
+
+        await client.query('COMMIT');
+        console.log('Producto insertado:', result.rows[0]);
         res.status(200).json({ message: 'Producto agregado al carrito exitosamente' });
     } catch (error) {
         console.error(error.message);
+
+        if (client) {
+            await client.query('ROLLBACK'); // Si ocurre un error, realizar un rollback
+            client.release();
+        }
+
         res.status(500).json({ message: 'Error al agregar el producto al carrito' });
+    } finally {
+        if (client) {
+            client.release();
+        }
     }
 });
+
+
 
 router.post('/user/car', async (req, res) => {
     const { id_usuario } = req.body;
@@ -133,7 +236,6 @@ router.delete('/user/car/Delete_element', async (req, res) => {
     if (!resultadoValidacion || resultadoValidacion.id_usuario !== id_usuario) {
         return res.status(401).json({ message: 'Acceso no autorizado' });
     }
-
     try {
         const client = await db.connect();
         const result = await client.query(
@@ -148,7 +250,27 @@ router.delete('/user/car/Delete_element', async (req, res) => {
         res.status(500).json({ message: 'Error interno del servidor' });
     }
 });
-
+router.post('/perfil_usuario', async (req, res) => {
+    const { id_usuario } = req.body;
+    const resultadoValidacion = validarToken(req, res);
+    if (!resultadoValidacion.id_usuario) {
+        return;
+    }
+    try {
+        const client = await db.connect();
+        const result = await client.query(
+            ` select correo, password, fecha_registro, fecha_nacimiento, nombre, apellido_paterno, apellido_materno, telefono from usuario u
+            join public.cliente c on u.id_usuario = c.id_usuario
+            join public.direccion d on c.id_cliente = d.id_cliente
+            where u.id_usuario = $1`, [id_usuario]);
+        const results = { 'data': (result) ? result.rows : null };
+        res.json(results);
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Error interno del servidor' }); // Cambia el mensaje de error
+    };
+})
 
 
 
