4b0ce0ba0eca3a8c693c0b3872f0d251528e6b76
GokuPrograming <verafrancomiguel1d@gmail.com>
2024-05-15 12:46:49 -0600

    se arreglaron los conflictos al hacer un merge

--- routes/carrito.js
+++ routes/carrito.js
@@ -1,30 +1,4 @@
-<<<<<<< HEAD
-const express = require('express');
-const router = express.Router();
-const db = require('../database/connection')  // Importa tu conexión a la base de datos aquí
-//const verificarToken = require('./verificaToken');  // Importa el middleware de verificación de token
 
-router.get('/carrito', verificarToken, async (req, res) => {
-    const { id_usuario } = req.usuario;  // Obtener id_usuario del token verificado
-
-    try {
-        const client = await db.connect();
-
-        // Consulta SQL para obtener todos los productos en el carrito del usuario
-        const result = await client.query('select * from carrito WHERE id_usuario = $1', [id_usuario]);
-
-        const results = { 'data': (result) ? result.rows : null };
-        res.json(results);
-
-        client.release();
-    } catch (err) {
-        console.error(err);
-        res.status(500).send("inicia sesion" + err);
-    }
-});
-
-module.exports = router;
-=======
 const express = require('express');
 const router = express.Router();
 const db = require('../database/connection')  // Importa tu conexión a la base de datos aquí
@@ -52,4 +26,4 @@ router.get('/carrito', verificarToken, async (req, res) => {
 
 
 module.exports = router;
->>>>>>> fcaffa2ab540678498f91c844122c1d255cfb4f6
+
--- routes/usuario.js
+++ routes/usuario.js
@@ -1,69 +1,4 @@
-<<<<<<< HEAD
-const express = require('express')
-const jwt = require('jsonwebtoken');  // Importa jsonwebtoken
-const db = require('../database/connection')
-const router = express.Router()
 
-
-router.get('/usuario', async (req, res) => {
-    try {
-        const client = await db.connect();
-        const result = await client.query('select * from usuario');
-        const results = { 'data': (result) ? result.rows : null };
-        res.json(results);
-        client.release();
-    } catch (err) {
-        console.error(err);
-        res.send("Error " + err);
-    }
-});
-router.get('/usuario/:id', async (req, res) => {
-    const id_usuario = req.params.id;  // Obtener el id_usuario desde los parámetros de la URL
-
-    try {
-        const client = await db.connect();
-        // Consulta SQL modificada para filtrar por id_usuario
-        const result = await client.query('SELECT * FROM usuario WHERE id_usuario = $1', [id_usuario]);
-        const results = { 'data': (result) ? result.rows : null };
-        res.json(results);
-
-        client.release();
-    } catch (err) {
-        console.error(err);
-        res.send("Error " + err);
-    }
-});
-
-router.post('/usuario/login', async (req, res) => {
-    const { correo, password } = req.body;  // Obtener correo y password desde el cuerpo de la solicitud
-
-    try {
-        const client = await db.connect();
-
-        // Consulta SQL para verificar las credenciales del usuario
-        const result = await client.query('SELECT * FROM usuario WHERE correo = $1 AND password = $2', [correo, password]);
-
-        if (result.rows.length > 0) {
-            const user = result.rows[0];
-
-            // Generar un token JWT
-            const token = jwt.sign({ id_usuario: user.id_usuario, correo: user.correo }, 'secreto', { expiresIn: '1h' });  // 'secreto' debe ser una cadena segura en la vida real
-
-            res.json({ 'message': 'Inicio de sesión exitoso', 'token': token });
-        } else {
-            res.status(401).json({ 'message': 'Correo o contraseña incorrectos' });
-        }
-
-        client.release();
-    } catch (err) {
-        console.error(err);
-        res.status(500).send("Error " + err);
-    }
-});
-
-
-module.exports = router;
-=======
 const express = require('express')
 const crypto = require('crypto');
 const jwt = require('jsonwebtoken');  // Importa jsonwebtoken
@@ -209,4 +144,4 @@ router.get('/usuario/:id', async (req, res) => {
 
 */
 
->>>>>>> fcaffa2ab540678498f91c844122c1d255cfb4f6
+
